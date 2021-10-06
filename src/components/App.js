c065dd2a178a092f4a7639201b119aecc7ad891d
Arpit Srivastav <arpit.s@surya-soft.com>
2021-10-06 08:37:25 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBox from './SearchBox';
+
+const App = () => {
+    return (
+        <div className="ui component">
+            <SearchBox />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SearchBox extends React.Component {
+// onInputChange(event){
+//     console.log(event.target.value);
+// }
+
+    state = { term: '' };
+
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label><h1>Image Search</h1></label>
+                        <input 
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value})} 
+                        />
+                    </div>
+                </form>
+            </div >
+        );
+    }
+}
+
+export default SearchBox;
